25250267ca6a5288588806de5fe097b2a2fe4c94
Jared Teraoka <jteraoka13@gmail.com>
2022-08-13 09:40:22 -0700

    updated questions

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -5,7 +5,7 @@ import Auth from "../utils/auth";
 import { GET_USER_MATCHES } from "../utils/queries";
 
 const HomePage = () => {
-    const userId = Auth.getUserId()
+    const userId = Auth.getUserId();
     const { loading, data, error } = useQuery(GET_USER_MATCHES, { variables: { user: userId } });  
 
     return (
@@ -14,7 +14,7 @@ const HomePage = () => {
             {loading && <div>don't look at me yet</div>}
 
             {!loading && !error && <div>
-                {data.getUserMatches.map(match => <p>{`${match.name} ${match.score} ${match.email}`}</p>)}   
+                {data.getUserMatches.map(match => <p>{`${match.name} ${match.score}% ${match.email}`}</p>)}   
             </div>}
 
             {/**add survey link and email addressed for users */}
--- server/seeders/seed.js
+++ server/seeders/seed.js
@@ -1,16 +1,16 @@
 const db = require('../config/connection');
 const { User, Answers } = require('../models');
 const userSeeds = require('./userSeeds.json');
-const { COLORS, FRUITS, FTA } = require("../../client/src/utils/surveyChoices")
+const { ROLES, VALUES, IDENTITIES } = require("../../client/src/utils/surveyChoices")
 
 const generateRandomAnswers = async (user) => {
   const userDocument = await User.findOne({ username: user.username })
 
   await Answers.create({
     user: userDocument._id,
-    q1: COLORS[Math.floor(Math.random() * COLORS.length)],
-    q2: FRUITS[Math.floor(Math.random() * FRUITS.length)],
-    q3: FTA[Math.floor(Math.random() * FTA.length)]
+    q1: ROLES[Math.floor(Math.random() * ROLES.length)],
+    q2: VALUES[Math.floor(Math.random() * VALUES.length)],
+    q3: IDENTITIES[Math.floor(Math.random() * IDENTITIES.length)]
   })
 }
 
